a2bfa7f8506caf950b6aad36f32b19ab58e0cdd8
Kuanysh <260505kuka@gmail.com>
2024-04-23 22:14:32 +0500

    added bottom bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {TopBarComponent} from "./top-bar/top-bar.component";
 import {BodyComponent} from "./body/body.component";
+import {BottomBarComponent} from "./bottom-bar/bottom-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopBarComponent, BodyComponent],
+  imports: [RouterOutlet, TopBarComponent, BodyComponent, BottomBarComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
